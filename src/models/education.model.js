bdcaecd1f3698aca266c425726311efb8b75b441
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-01-28 15:15:27 +0100

    Update education model and peticiones routes

--- src/models/education.model.js
+++ src/models/education.model.js
@@ -3,12 +3,11 @@ const { Schema, model } = require("mongoose");
 const educationSchema = new Schema(
   {
     institucion: String,
-    titulo: String,
+    tituloObtenido: String,
     fechaInicio: String,
     fechaFin: Date,
+    actual: Boolean,
     descripcion: { type: String, maxlength: 500 },
-    skillTitle: String,
-    skillDescription: { type: String, maxlength: 300 },
     tecnologias: [{ type: String }],
   },
   { timestamps: true, versionKey: false }
